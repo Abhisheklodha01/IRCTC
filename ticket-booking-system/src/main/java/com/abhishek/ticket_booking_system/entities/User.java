63e8b3893ccdbbd38e4fa5f9c3513a9b9dc1e08c
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-21 16:47:14 +0530

    created user entity

---
+++ ticket-booking-system/src/main/java/com/abhishek/ticket_booking_system/entities/User.java
@@ -0,0 +1,12 @@
+package com.abhishek.ticket_booking_system.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+    private List<Ticket> bokkedTickets;
+    private String userId;
+
+}
