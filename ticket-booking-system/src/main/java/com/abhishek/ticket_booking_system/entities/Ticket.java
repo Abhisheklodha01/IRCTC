fddcf353bedae9ad3f060add2725c8d9d2ad3c91
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-21 16:47:35 +0530

    created ticket entity

---
+++ ticket-booking-system/src/main/java/com/abhishek/ticket_booking_system/entities/Ticket.java
@@ -0,0 +1,13 @@
+package com.abhishek.ticket_booking_system.entities;
+
+import java.sql.Date;
+
+public class Ticket {
+    private String ticketId;
+    private String userId;
+    private String source;
+    private String destination;
+    private Date travelDate;
+    private Train train;
+
+}
