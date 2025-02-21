402c46ba4950e7b46d3df6732968c99a895b54cd
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-21 16:47:46 +0530

    created user service

---
+++ ticket-booking-system/src/main/java/com/abhishek/ticket_booking_system/services/UserBookingService.java
@@ -0,0 +1,11 @@
+package com.abhishek.ticket_booking_system.services;
+
+import com.abhishek.ticket_booking_system.entities.User;
+
+public class UserBookingService {
+    
+    private User user;
+    public UserBookingService(User user) {
+         this.user = user;
+    }
+}
